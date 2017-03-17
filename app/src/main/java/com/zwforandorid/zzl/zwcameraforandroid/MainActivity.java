de38a4a46bf0f2ebc640ea4c21bfae037e71c55b
zzl <646172505@qq.com>
2017-03-17 15:05:33 +0800

    修改

--- app/src/main/java/com/zwforandorid/zzl/zwcameraforandroid/MainActivity.java
+++ app/src/main/java/com/zwforandorid/zzl/zwcameraforandroid/MainActivity.java
@@ -1,13 +1,50 @@
 package com.zwforandorid.zzl.zwcameraforandroid;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.zwforandorid.zzl.zwcamera.ZWCameraActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView xx = (TextView) findViewById(R.id.tv_test);
+        xx.setOnClickListener( this );
     }
+
+        @Override public void onClick(final View v) {
+            if (PermissionChecker.checkSelfPermission(this, Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA },
+                        v.getId());
+            } else {
+                startActivity(v.getId());
+            }
+        }
+
+        @Override public void onRequestPermissionsResult(int requestCode, String[] permissions,
+        int[] grantResults) {
+            if (grantResults.length != 1 || grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startActivity(requestCode);
+            } else {
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
+        }
+
+    private void startActivity(int id) {
+
+        Intent xxx =  new Intent(this, ZWCameraActivity.class);
+        startActivity( xxx );
+        }
 }
---
+++ zwcamera/src/main/java/com/zwforandorid/zzl/zwcamera/ZWCameraActivity.java
@@ -0,0 +1,98 @@
+package com.zwforandorid.zzl.zwcamera;
+
+import android.hardware.Camera;
+import android.opengl.GLSurfaceView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.zwforandorid.zzl.zwcamera.utils.CameraHelper;
+
+import jp.co.cyberagent.android.gpuimage.GPUImage;
+
+public class ZWCameraActivity extends AppCompatActivity {
+
+    private GPUImage mGPUImage;
+    private CameraHelper mCameraHelper;
+    private CameraLoader mCamera;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zwcamera);
+
+        mGPUImage = new GPUImage(this);
+        mGPUImage.setGLSurfaceView((GLSurfaceView) findViewById(R.id.surfaceView));
+
+        mCameraHelper = new CameraHelper(this);
+        mCamera = new CameraLoader();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCamera.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        mCamera.onPause();
+        super.onPause();
+    }
+
+    private class CameraLoader {
+
+        private int mCurrentCameraId = 0;
+        private Camera mCameraInstance;
+
+        public void onResume() {
+            setUpCamera(mCurrentCameraId);
+        }
+
+        public void onPause() {
+            releaseCamera();
+        }
+
+        public void switchCamera() {
+            releaseCamera();
+            mCurrentCameraId = (mCurrentCameraId + 1) % mCameraHelper.getNumberOfCameras();
+            setUpCamera(mCurrentCameraId);
+        }
+
+        private void setUpCamera(final int id) {
+            mCameraInstance = getCameraInstance(id);
+            Camera.Parameters parameters = mCameraInstance.getParameters();
+            // TODO adjust by getting supportedPreviewSizes and then choosing
+            // the best one for screen size (best fill screen)
+            if (parameters.getSupportedFocusModes().contains(
+                    Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
+                parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+            }
+            mCameraInstance.setParameters(parameters);
+
+            int orientation = mCameraHelper.getCameraDisplayOrientation(
+                    ZWCameraActivity.this, mCurrentCameraId);
+            CameraHelper.CameraInfo2 cameraInfo = new CameraHelper.CameraInfo2();
+            mCameraHelper.getCameraInfo(mCurrentCameraId, cameraInfo);
+            boolean flipHorizontal = cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_FRONT;
+            mGPUImage.setUpCamera(mCameraInstance, orientation, flipHorizontal, false);
+        }
+
+        /** A safe way to get an instance of the Camera object. */
+        private Camera getCameraInstance(final int id) {
+            Camera c = null;
+            try {
+                c = mCameraHelper.openCamera(id);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return c;
+        }
+
+        private void releaseCamera() {
+            mCameraInstance.setPreviewCallback(null);
+            mCameraInstance.release();
+            mCameraInstance = null;
+        }
+    }
+}
