ed207f13fbdb0de780b13edbf7ccc46d2b6af0e2
zzl <646172505@qq.com>
2017-03-17 14:11:56 +0800

    第一次提交

---
+++ app/src/main/java/com/zwforandorid/zzl/zwcameraforandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zwforandorid.zzl.zwcameraforandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
